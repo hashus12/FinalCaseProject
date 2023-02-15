fa7b0b4fa817078a4e3b7bcdd802291cb27b0e5b
Hasan Hüseyin Özgen <53178769+hashus12@users.noreply.github.com>
2023-02-15 15:46:41 +0300

    can return the result of the loan application request.my sql connect. 15.02.2023

--- src/main/java/com/hasan/finalcaseproject/controllers/LoanApplicationController.java
+++ src/main/java/com/hasan/finalcaseproject/controllers/LoanApplicationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/loanapplication")
+@RequestMapping("/forms")
 public class LoanApplicationController {
     private LoanApplicationService loanApplicationService;
     private CustomerService customerService;
